52137dd6be6ce3cb211c27b540123158a5912447
Wilk Bartlomiej <wilkbartlomiej26@gmail.com>
2018-06-11 22:27:35 +0200

    Changes and comments out

--- AutoTestFramework/Program.cs
+++ AutoTestFramework/Program.cs
@@ -6,11 +6,12 @@ namespace AutoTestFramework
 
     public class Program
     {
-        IAlert alert;
+        //Alert for testing login form
+        //IAlert alert;
 
         static void Main()
         {
-            //navigating to Login Form through post
+            //Navigating to Login Form through post
             //Thread.Sleep(1000);
 
             //Driver.driver.Navigate().GoToUrl("http://testing.todvachev.com/");
@@ -22,27 +23,29 @@ namespace AutoTestFramework
             //Driver.driver.Quit();
         }
 
-        [SetUp]
-        public void Initialize()
-        {
-            Actions.InitializeDriver();
-        }
 
-        [Test]
-        public void ValidLogin()
-        {
-            NavigateTo.LoginFormThroughTheMenu();
-            Actions.FillLoginForm(Config.Credentials.Valid.Username, Config.Credentials.Valid.Password, Config.Credentials.Valid.RepeatPassword);
+        //testing login form
+        //[SetUp]
+        //public void Initialize()
+        //{
+        //    Actions.InitializeDriver();
+        //}
 
-            alert = Driver.driver.SwitchTo().Alert();
-            Assert.AreEqual(Config.AlertMessages.SuccessfulLogin, alert.Text);
-            alert.Accept();
-        }
+        //[Test]
+        //public void ValidLogin()
+        //{
+        //    NavigateTo.LoginFormThroughTheMenu();
+        //    Actions.FillLoginForm(Config.Credentials.Valid.Username, Config.Credentials.Valid.Password, Config.Credentials.Valid.RepeatPassword);
 
-        [TearDown]
-        public void CleanUp()
-        {
-            Driver.driver.Quit();
-        }
+        //    alert = Driver.driver.SwitchTo().Alert();
+        //    Assert.AreEqual(Config.AlertMessages.SuccessfulLogin, alert.Text);
+        //    alert.Accept();
+        //}
+
+        //[TearDown]
+        //public void CleanUp()
+        //{
+        //    Driver.driver.Quit();
+        //}
     }
 }
